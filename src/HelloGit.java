5bf30a098cf5cc7c887fa46e7986313f4f09fc0f
LindaDeng <LindaDeng@apcs.balboa.sfusd.edu>
2015-02-26 15:08:34 +0000

    Initail Commit

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,6 +1,6 @@
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello!");
-		System.out.println("Hi, my name is Linda.");
+		System.out.println("Hi, my name is Linda Deng.");
 	}
 }
